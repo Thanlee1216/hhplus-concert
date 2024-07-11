de37c81438dea2f8e0c7ca66456bcff843fb33da
leetaehan <lth9312@gmail.com>
2024-07-12 08:41:51 +0900

    Feat: Business - service

--- src/main/java/com/hhplus/concert/business/service/BalanceService.java
+++ src/main/java/com/hhplus/concert/business/service/BalanceService.java
@@ -1,7 +1,21 @@
 package com.hhplus.concert.business.service;
 
+import com.hhplus.concert.business.domain.UserDomain;
+import com.hhplus.concert.business.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BalanceService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserDomain findBalanceById(Long userId) {
+        return userRepository.findById(userId);
+    }
+
+    public UserDomain updateBalance(UserDomain userDomain) {
+        return userRepository.save(userDomain);
+    }
 }
