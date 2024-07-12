1d42aa567692b20de09dde6ca12183813817dd81
leetaehan <lth9312@gmail.com>
2024-07-12 17:11:56 +0900

    Feat: application layer

---
+++ src/main/java/com/hhplus/concert/application/mapper/QueueFacadeMapper.java
@@ -0,0 +1,15 @@
+package com.hhplus.concert.application.mapper;
+
+import com.hhplus.concert.application.dto.QueueFacadeDTO;
+import com.hhplus.concert.business.domain.QueueDomain;
+
+public class QueueFacadeMapper {
+
+    public static QueueDomain toQueueDomain(QueueFacadeDTO facadeDTO) {
+        return new QueueDomain(facadeDTO.queueNumber(), facadeDTO.userId(), facadeDTO.status(), facadeDTO.userQueueCount());
+    }
+
+    public static QueueFacadeDTO toQueueFacadeDTO(QueueDomain queueDomain) {
+        return new QueueFacadeDTO(queueDomain.queueNumber(), queueDomain.userId(), queueDomain.status(), queueDomain.userQueueCount());
+    }
+}
