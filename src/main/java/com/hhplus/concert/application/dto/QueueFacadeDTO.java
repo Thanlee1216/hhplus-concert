d6c3e3baf0731b49259d1ba793c57d54a2401587
LeeTaeHan <lth9312@gmail.com>
2024-07-12 21:00:09 +0900

    Feat: 대기열 만료

--- src/main/java/com/hhplus/concert/application/dto/QueueFacadeDTO.java
+++ src/main/java/com/hhplus/concert/application/dto/QueueFacadeDTO.java
@@ -2,9 +2,13 @@ package com.hhplus.concert.application.dto;
 
 import com.hhplus.concert.business.constant.QueueStatusType;
 
+import java.sql.Timestamp;
+
 public record QueueFacadeDTO(
         Long queueNumber,
         Long userId,
+        Timestamp createdAt,
+        Timestamp activeAt,
         QueueStatusType status,
         Long userQueueCount
 ) {
