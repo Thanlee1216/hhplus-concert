4d006b60406b41c235fa5cde0c79ae8294442eaa
LeeTaeHan <lth9312@gmail.com>
2024-07-25 00:11:50 +0900

    Test: Pessimistic Lock Test

--- src/main/java/com/hhplus/concert/infrastructure/persistence/dataaccess/ConcertSeatJpaRepository.java
+++ src/main/java/com/hhplus/concert/infrastructure/persistence/dataaccess/ConcertSeatJpaRepository.java
@@ -1,10 +1,16 @@
 package com.hhplus.concert.infrastructure.persistence.dataaccess;
 
 import com.hhplus.concert.infrastructure.entity.ConcertSeatEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.List;
 
 public interface ConcertSeatJpaRepository extends JpaRepository<ConcertSeatEntity, Long> {
     List<ConcertSeatEntity> findByConcertIdAndConcertOptionId(Long concertId, Long concertOptionId);
+
+    @Override
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    <S extends ConcertSeatEntity> S save(S entity);
 }
