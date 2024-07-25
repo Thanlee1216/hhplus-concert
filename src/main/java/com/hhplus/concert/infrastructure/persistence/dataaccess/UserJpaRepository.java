ccfb6f0569a46fd6b8a48fb8ea9948c9beea2c63
LeeTaeHan <lth9312@gmail.com>
2024-07-26 04:29:40 +0900

    Fix: Transactional을 Facade -> Service로 내리고 비관적 락 적용

--- src/main/java/com/hhplus/concert/infrastructure/persistence/dataaccess/UserJpaRepository.java
+++ src/main/java/com/hhplus/concert/infrastructure/persistence/dataaccess/UserJpaRepository.java
@@ -1,7 +1,14 @@
 package com.hhplus.concert.infrastructure.persistence.dataaccess;
 
 import com.hhplus.concert.infrastructure.entity.UsersEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import java.util.Optional;
 
 public interface UserJpaRepository extends JpaRepository<UsersEntity, Long> {
+    @Override
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<UsersEntity> findById(Long aLong);
 }
